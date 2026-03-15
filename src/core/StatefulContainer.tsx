5e696e836f79ab6c3374c7c365e61120d8b76ff8
Heungrok <mabookyggl@gmail.com>
2026-03-15 20:54:30 +0900

    docs: StatefulContainer 문서 추가

--- src/core/StatefulContainer.tsx
+++ src/core/StatefulContainer.tsx
@@ -5,6 +5,13 @@ export type StatefulContainerProps = ComponentProps<"div"> & {
     usesRipple?: boolean;
 }
 
+/**
+ * StatefulContainer 컴포넌트는 인터랙션이 가능한 컴포넌트를 빠르게 구현하도록 돕는 컴포넌트입니다.
+ * Material Design 3에서 컴포넌트를 구성하는 세 레이어(Container, State, Content) 중 
+ * Container 레이어와 State 레이어를 렌더링하며, 자식 요소로써 Content 레이어를 렌더링합니다.
+ * State 레이어의 경우 Hover, Focus, Press, Drag 상태에 따른 스타일링이 자동으로 적용되며,
+ * 레이어의 색상으로는 currentcolor(현재 텍스트 색상)가 사용됩니다.
+ */
 export function StatefulContainer({
     ref,
     className,
